fc31f4e3d9eec788b6c96bb67e7098823c1ea44e
Thai Long <d-thailong@pasonatech.vn>
2025-03-10 14:45:52 +0700

    Add Rewind-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import type { ThemeContextType } from "@rewind-ui/core";
+import { ThemeProvider, useTheme } from "@rewind-ui/core";
+
+import { ResourceAvatar } from "./components";
+import { avatarStyles } from "./theme";
+import { Furniture } from "./types";
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const defaultTheme = useTheme();
+
+  const themeContext: ThemeContextType = {
+    theme: {
+      components: {
+        ...defaultTheme.components,
+        Avatar: { ...defaultTheme.components.Avatar, ...avatarStyles },
+      },
+    },
+  };
+
+  return (
+    <ThemeProvider value={themeContext}>
+      <ResourceAvatar resource={Furniture.NEATLY_ARRANGED_TABLE} />
+      <ResourceAvatar resource={Furniture.RABBIT_SCULPTURE_CHAIR} />
+      <ResourceAvatar resource={Furniture.RABBIT_SCULPTURE_DRAWER} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
