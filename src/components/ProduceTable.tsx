7e91c104409ee09980b59de94127d60fa7df9794
Thai Long <d-thailong@pasonatech.vn>
2025-03-13 09:25:38 +0700

    Refactor code

--- src/components/ProduceTable.tsx
+++ src/components/ProduceTable.tsx
@@ -1,3 +1,4 @@
+import type { CSSProperties } from "react";
 import React from "react";
 
 import type { Product, Resource } from "../types";
@@ -9,34 +10,46 @@ type Props = {
   referenceResource: Resource;
 };
 
+const itemSpace: Size = { w: 96, h: 96 };
+const itemSize: Size = { w: 64, h: 64 };
+const offset: Position = { x: (itemSpace.w - itemSize.w) / 2, y: (itemSpace.h - itemSize.h) / 2 };
+
+function _s(size: Size): CSSProperties {
+  return { width: size.w, height: size.h };
+}
+
+function _p(pos: Position): CSSProperties {
+  return { top: pos.y, left: pos.x };
+}
+
 export function ProduceTable(props: Props) {
   const { referenceResource } = props;
 
   const ingredientSide = calIngredientNode(referenceResource);
   const produceSide = calProduceNode(referenceResource);
 
-  const sizeRequired = {
-    width: ingredientSide.size[0] + produceSide.size[0] - 96,
-    height: Math.max(ingredientSide.size[1], produceSide.size[1]),
+  const sizeRequired: Size = {
+    w: ingredientSide.size.w + produceSide.size.w - itemSpace.w,
+    h: Math.max(ingredientSide.size.h, produceSide.size.h),
   };
 
-  const refPos = {
-    top: (sizeRequired.height - 96) / 2 + 16,
-    left: sizeRequired.width - produceSide.size[0] + 16,
+  const refPos: Position = {
+    x: sizeRequired.w - produceSide.size.w + offset.x,
+    y: (sizeRequired.h - itemSpace.h) / 2 + offset.y,
   };
 
-  const ingredientOrigin: [number, number] = [0, (sizeRequired.height - ingredientSide.size[1]) / 2];
+  const ingredientOrigin: [number, number] = [0, (sizeRequired.h - ingredientSide.size.h) / 2];
   const ingredientNodes = resolveIngredientNode(ingredientSide, ingredientOrigin);
 
   const produceOrigin: [number, number] = [
-    ingredientSide.size[0] - 96,
-    (sizeRequired.height - produceSide.size[1]) / 2,
+    ingredientSide.size.w - itemSpace.w,
+    (sizeRequired.h - produceSide.size.h) / 2,
   ];
   const produceNodes = resolveProduceNode(produceSide, produceOrigin);
 
   return (
-    <div className='relative bg-slate-200' style={sizeRequired}>
-      <div className='absolute' style={refPos}>
+    <div className='relative bg-slate-200' style={_s(sizeRequired)}>
+      <div className='absolute' style={_p(refPos)}>
         {ingredientSide.jsx}
       </div>
       {ingredientNodes}
@@ -46,7 +59,8 @@ export function ProduceTable(props: Props) {
 }
 
 type NodeInfo = {
-  size: [number, number];
+  size: Size;
+  pos: Position;
   jsx: React.ReactElement;
   child?: NodeInfo[];
 };
@@ -54,19 +68,19 @@ type NodeInfo = {
 function calProduceNode(resource: Resource): NodeInfo {
   if (resource.ingredientFor.length === 0) {
     return {
-      size: [96, 96],
+      size: itemSpace,
+      pos: { x: 0, y: 0 },
       jsx: isProduct(resource) ? <ProductItem product={resource} /> : <ResourceItem resource={resource} />,
     };
   }
 
   const childNode = resource.ingredientFor.map(calProduceNode);
-  const childSize = [
-    childNode.reduce((maxWidth, cur) => (cur.size[0] > maxWidth ? cur.size[0] : maxWidth), 0),
-    childNode.reduce((height, cur) => cur.size[1] + height, 0),
-  ];
+  const childWidth = childNode.reduce((maxWidth, cur) => (cur.size.w > maxWidth ? cur.size.w : maxWidth), 0);
+  const childHeight = childNode.reduce((height, cur) => cur.size.h + height, 0);
 
   return {
-    size: [96 + childSize[0], Math.max(96, childSize[1])],
+    size: { w: itemSpace.w + childWidth, h: Math.max(itemSpace.h, childHeight) },
+    pos: { x: 0, y: 0 },
     jsx: isProduct(resource) ? <ProductItem product={resource} /> : <ResourceItem resource={resource} />,
     child: childNode,
   };
@@ -75,19 +89,19 @@ function calProduceNode(resource: Resource): NodeInfo {
 function calIngredientNode(resource: Resource | Product): NodeInfo {
   if (!isProduct(resource)) {
     return {
-      size: [96, 96],
+      size: itemSpace,
+      pos: { x: 0, y: 0 },
       jsx: <ResourceItem resource={resource} />,
     };
   }
 
   const childNode = resource.ingredient.map((v) => calIngredientNode(v.ingredient));
-  const childSize = [
-    childNode.reduce((maxWidth, cur) => (cur.size[0] > maxWidth ? cur.size[0] : maxWidth), 0),
-    childNode.reduce((height, cur) => cur.size[1] + height, 0),
-  ];
+  const childWidth = childNode.reduce((maxWidth, cur) => (cur.size.w > maxWidth ? cur.size.w : maxWidth), 0);
+  const childHeight = childNode.reduce((height, cur) => cur.size.h + height, 0);
 
   return {
-    size: [96 + childSize[0], Math.max(96, childSize[1])],
+    size: { w: itemSpace.w + childWidth, h: Math.max(itemSpace.h, childHeight) },
+    pos: { x: 0, y: 0 },
     jsx: <ProductItem product={resource} />,
     child: childNode,
   };
@@ -98,16 +112,17 @@ function resolveProduceNode(info: NodeInfo, origin: [number, number], includePar
   const jsxs = info.child
     ? info.child.flatMap((v) => {
         const child = resolveProduceNode(v, [origin[0] + 96, height], true);
-        height += v.size[1];
+        height += v.size.h;
         return child;
       })
     : [];
 
   if (includeParent) {
-    const size = info.size;
-    const pos = { left: origin[0] + 16, top: origin[1] + (size[1] - 96) / 2 + 16 };
+    const { size, pos } = info;
+    pos.x = origin[0] + offset.x;
+    pos.y = origin[1] + (size.h - 96) / 2 + offset.y;
     jsxs.push(
-      <div className='absolute' style={pos}>
+      <div className='absolute' style={_p(pos)}>
         {info.jsx}
       </div>,
     );
@@ -124,17 +139,18 @@ function resolveIngredientNode(
   let top = origin[1];
   const jsxs = info.child
     ? info.child.flatMap((v) => {
-        const child = resolveIngredientNode(v, [origin[0] + (info.size[0] - v.size[0] - 96), top], true);
-        top += v.size[1];
+        const child = resolveIngredientNode(v, [origin[0] + (info.size.w - v.size.w - 96), top], true);
+        top += v.size.h;
         return child;
       })
     : [];
 
   if (includeParent) {
-    const size = info.size;
-    const pos = { left: origin[0] + (size[0] - 96) + 16, top: origin[1] + (size[1] - 96) / 2 + 16 };
+    const { size, pos } = info;
+    pos.x = origin[0] + (size.w - 96) + offset.x;
+    pos.y = origin[1] + (size.h - 96) / 2 + offset.y;
     jsxs.push(
-      <div className='absolute' style={pos}>
+      <div className='absolute' style={_p(pos)}>
         {info.jsx}
       </div>,
     );
