c81adc9b01955cd3944aa2e1616f292360540276
Thai Long <d-thailong@pasonatech.vn>
2025-03-18 15:30:48 +0700

    Add ItemsPage

---
+++ src/components/ItemCard.tsx
@@ -0,0 +1,44 @@
+import { Card, Image, Text } from "@rewind-ui/core";
+import { useNavigate } from "react-router";
+
+import { CoinHandover } from "../svgs/CoinHandover";
+import { CoinSvg } from "../svgs/CoinSvg";
+import type { Resource } from "../types";
+import { numberFormat } from "../utils";
+
+type Props = {
+  item: Resource;
+};
+
+export function ItemCard(props: Props) {
+  const { item } = props;
+
+  const navigate = useNavigate();
+
+  console.log("Render", ItemCard.name, item.name);
+  return (
+    <Card
+      className='w-60'
+      shadow='md'
+      onClick={() => navigate(`/magical-ink/items/${item.category.filename}/${item.filename}`)}
+    >
+      <Image caption={item.name} mode='light' src={item.img} alt={item.name} className='w-60 h-60 object-contain' />
+
+      <Card.Body className='flex flex-col justify-start items-start gap-1'>
+        <Text variant='h6' as='p'>
+          {item.name}
+        </Text>
+
+        <div className='flex flex-row gap-1 justify-start items-center'>
+          <CoinSvg size={24} />
+          <Text>{numberFormat(item.price)}</Text>
+        </div>
+
+        <div className='flex flex-row gap-1 justify-start items-center'>
+          <CoinHandover size={24} />
+          <Text>{numberFormat(item.wishPrice)}</Text>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,5 +1,49 @@
+import { Text } from "@rewind-ui/core";
+
+import { ItemCard } from "../components/ItemCard";
+import {
+  AlchemizeProduct,
+  CookingProduct,
+  FarmProduct,
+  Furniture,
+  MaterialTool,
+  MonsterItem,
+  NatureItem,
+  Ore,
+  TreeFarmProduct,
+  WorldMonsterItem,
+} from "../types";
+
+const content = [
+  ["Nature", NatureItem.all()],
+  ["Farm", FarmProduct.all()],
+  ["Tree Farm", TreeFarmProduct.all()],
+  ["Cooking", CookingProduct.all()],
+  ["Alchemize", AlchemizeProduct.all()],
+  ["Materials and Tools", MaterialTool.all()],
+  ["Ore", Ore.all()],
+  ["Furniture", Furniture.all()],
+  ["World Monster Drop", WorldMonsterItem.all()],
+  ["Monster Drop", MonsterItem.all()],
+] as const;
+
 export function ItemsPage() {
   console.log("Render", ItemsPage.name);
 
-  return <div>Items Page</div>;
+  return (
+    <div className='flex flex-col gap-4 justify-start items-stretch'>
+      {content.map(([title, items]) => (
+        <div key={title}>
+          <Text variant='h3' className='border-b-2 border-opacity-60 border-blue-400 px-2'>
+            {title}
+          </Text>
+          <div className='flex flex-row flex-wrap gap-2 py-2'>
+            {items.map((v) => (
+              <ItemCard key={v.filename} item={v} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
